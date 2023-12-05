e7890186e3363a876f357a1089a406289c2508dd
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-12-05 17:54:19 +0000

    Updated App component structure and integrated ListaDeTareas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
+import freeCodeCampLogo from "./images/freecodecamp-logo.png";
+import ListaDeTareas from "./components/ListaDeTareas";
 
 function App() {
-  return <div className="App">React</div>;
+  return (
+    <div className="app-todo-list">
+      <div className="freecodecamp-logo-contenedor">
+        <img src={freeCodeCampLogo} className="freecodecamp-logo" />
+      </div>
+      <div className="tareas-lista-principal">
+        <h1>Mis tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+// Todo
+/* 1) Hacer que las tareas sean almacenadas depués de recargar la página
+ */
